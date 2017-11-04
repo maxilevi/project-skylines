95dd65a7f3f3a9251bb8d2adfdcfd3fc18be1987
Zaphyk <zaphyk@live.com>
2017-11-04 00:33:41 -0300

    more works on game generation

---
+++ Assets/Generation/CommonQueue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+namespace Assets.Generation
+{
+
+    public class CommonQueue
+    {
+        public const int NumThreads = 2;
+        public Thread[] Threads = new Thread[NumThreads];
+
+        public CommonQueue()
+        {
+            for (int i = 0; i < this.Threads.Length; i++)
+            {
+                //this.Threads[i] = new Thread();
+            }
+        }
+
+    }
+
+    public enum QueueType
+    {
+        Generate,
+        Build
+    }
+}
---
+++ Assets/World.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Generation;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+
+	private readonly Dictionary<Vector3, Chunk> _chunks = new Dictionary<Vector3, Chunk>();
+    private readonly CommonQueue _queue = new CommonQueue();
+
+    public void AddChunk(Vector3 Offset, Chunk Chunk)
+    {
+        if (!this._chunks.ContainsKey(Offset))
+        {
+            this._chunks.Add(Offset, Chunk);
+            this._queue.Add(Chunk, QueueType.Generate);
+        }
+    }
+    public void RemoveChunk(Vector3 Offset) { }
+
+}
