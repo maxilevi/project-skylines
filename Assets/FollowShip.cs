40c488df5cb05d307fde915dffe2dcd4bc6d3564
Zaphyk <zaphyk@live.com>
2017-11-10 02:15:01 -0300

    added new ship controls

---
+++ Assets/FollowShip.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowShip : MonoBehaviour {
+
+	public GameObject TargetShip;
+	public Vector3 Offset;
+
+	void Update () {
+		this.transform.position = Lerp (this.transform.position, TargetShip.transform.position + TargetShip.transform.forward * Offset.z + TargetShip.transform.up * Offset.y, Time.deltaTime * 16f);
+		this.transform.LookAt(TargetShip.transform.position + Vector3.up, Vector3.up);
+        //this.transform.rotation = Quaternion.Slerp(this.transform.rotation, TargetShip.transform.rotation, Time.deltaTime * 32f);
+    }
+
+	public Vector3 Lerp(Vector3 A, Vector3 B, float C){
+		return new Vector3 (Mathf.Lerp(A.x, B.x, C), Mathf.Lerp(A.y, B.y, C), Mathf.Lerp(A.z, B.z, C)  );
+	}
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float Speed = 16;
+	public float TurnSpeed = 3f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.parent.position += transform.forward * Time.deltaTime * 4 * Speed;
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(Vector3.zero), Time.deltaTime * 1.5f);
+		Debug.Log(transform.rotation.eulerAngles + " | " + transform.localRotation.eulerAngles);
+        if (Input.GetKey(KeyCode.W))
+        {
+			transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles + Vector3.right * Time.deltaTime * 64f * TurnSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+			transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles - Vector3.right * Time.deltaTime * 64f * TurnSpeed);
+        }
+
+        if (Input.GetKey (KeyCode.A)) {
+			transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles + Vector3.forward * Time.deltaTime * 64f * TurnSpeed);
+			transform.parent.Rotate(- Vector3.up * Time.deltaTime * 64f * TurnSpeed );
+        }
+
+		if (Input.GetKey (KeyCode.D)) {
+			transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles - Vector3.forward * Time.deltaTime * 64f * TurnSpeed);
+			transform.parent.Rotate( Vector3.up * Time.deltaTime * 64f * TurnSpeed );
+
+        }
+	}
+}
