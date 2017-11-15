c66c1628bbb9cefc171cb2b2f6fdb292e5b12bb9
Zaphyk <zaphyk@live.com>
2017-11-14 21:41:10 -0300

    renamed GraphicsOptions.cs to Options.cs

---
+++ Assets/Options.cs
@@ -0,0 +1,28 @@
+﻿/* Copyright (C) Luaek - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Maxi Levi <maxilevi@live.com>, November 2017
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets
+{
+
+    public static class Options
+    {
+
+        public static int ChunkLoaderRadius { get; set; }
+		public static bool Lod { get; set; }
+		public static bool Invert { get; set;}
+
+        static Options()
+        {
+            ChunkLoaderRadius = 10;
+			Lod = true;
+			Invert = true;
+        }
+    }
+}
