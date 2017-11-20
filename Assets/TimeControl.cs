34339c6c7ecf17be771b8b92acf62dfd30d2bbb1
Zaphyk <zaphyk@live.com>
2017-11-20 17:40:52 -0300

    removed copyright info from files

--- Assets/TimeControl.cs
+++ Assets/TimeControl.cs
@@ -1,8 +1,6 @@
-﻿/* Copyright (C) Luaek - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Maxi Levi <maxilevi@live.com>, November 2017
- */
+﻿/*
+ *Written by Maxi Levi <maxilevi@live.com>, November 2017
+*/
 
 using System.Linq;
 using System.Collections;
